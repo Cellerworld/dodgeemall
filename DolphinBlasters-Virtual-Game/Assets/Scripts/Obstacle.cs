6324c1ff6a95f59a414bb990ef89aa18fdbd2afc
DESKTOP-P1DU2EF\kraem <kraemerfa@t-online.de>
2018-05-15 15:05:12 +0200

    implemented 3 characters

--- DolphinBlasters-Virtual-Game/Assets/Scripts/Obstacle.cs
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/Obstacle.cs
@@ -18,7 +18,7 @@ public class Obstacle : MonoBehaviour {
 
     private void Update()
     {
-        _canvas.transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        _canvas.transform.position = new Vector3(transform.position.x, -1f, transform.position.z);
     }
 
     private void OnCollisionEnter(Collision collision)
