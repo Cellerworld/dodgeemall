e372f324e36284bc9535d013f4a54a42fff02fa2
LAPTOP-UBUK1R6P\Pascal <pascal-diroll@gmx.de>
2018-05-16 10:34:53 +0200

    ASDFGH

--- DolphinBlasters-Virtual-Game/Assets/Scripts/MatchModeButtonBehavior.cs
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/MatchModeButtonBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class MatchSelectionButtonBehavior : MonoBehaviour {
+public class MatchModeButtonBehavior : MonoBehaviour {
 
 	private bool _is_Selected;
 	private TextMeshProUGUI _current_Text;
