e2e9c5c73d134a240ced81b3f55047e32162e79e
LAPTOP-UBUK1R6P\Pascal <pascal-diroll@gmx.de>
2018-05-21 23:32:58 +0200

    first half transition

--- DolphinBlasters-Virtual-Game/Assets/Scripts/ButtonBehavior.cs
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/ButtonBehavior.cs
@@ -15,7 +15,19 @@ public class ButtonBehavior : MonoBehaviour {
 	[SerializeField]
 	private GameObject _goBackObject;
 
+	[SerializeField]
+	private Animator[] animator = new Animator[2];
+
 	private int i = 0;
+
+	// private bool _animation_has_played;
+	[SerializeField]
+	private GameObject _enable;
+	[SerializeField]
+	private GameObject _disable;
+	[SerializeField]
+	private GameObject _select;
+
 	// Use this for initialization
 	void Start () {
 		Cursor.visible = false;
@@ -30,26 +42,34 @@ public class ButtonBehavior : MonoBehaviour {
 			{
 				return;
 			}
-			i++;
-			Debug.Log (i);
-			if (_goBackObject.transform.parent.tag == "lores") {
-				Debug.Log (_goBackObject.transform.parent.gameObject.name);
-				_goBackObject.transform.parent.gameObject.SetActive (true);
-				eventSystem.SetSelectedGameObject (_goBackObject);
-				gameObject.transform.parent.gameObject.SetActive (false);
-				return;
-			} 
-			else {
-				Debug.Log (_goBackObject.transform.parent.gameObject.name);
-				_goBackObject.transform.parent.gameObject.SetActive (true);
-				eventSystem.SetSelectedGameObject (_goBackObject);
-				gameObject.transform.parent.gameObject.SetActive (false);
-			}
+			//			i++;
+			//			Debug.Log (i);
+			//			if (_goBackObject.transform.parent.tag == "lores") {
+			//				Debug.Log (_goBackObject.transform.parent.gameObject.name);
+			//				_goBackObject.transform.parent.gameObject.SetActive (true);
+			//				eventSystem.SetSelectedGameObject (_goBackObject);
+			//				gameObject.transform.parent.gameObject.SetActive (false);
+			//				return;
+			//			} 
+			//else {
+			//Debug.Log (_goBackObject.transform.parent.gameObject.name);
+
+			StartCoroutine ("goBack");
+			//				_goBackObject.transform.parent.gameObject.SetActive (true);
+			//				eventSystem.SetSelectedGameObject (_goBackObject);
+			//				gameObject.transform.parent.gameObject.SetActive (false);
+			//}
 
 			//StartCoroutine ("goBack");
 
 		}
-		
+
+	}
+
+	public void goToNext()
+	{
+
+		StartCoroutine ("goNext");
 	}
 
 	public void increaseOnHover(Button other)
@@ -67,9 +87,40 @@ public class ButtonBehavior : MonoBehaviour {
 		eventSystem.SetSelectedGameObject (newHighlightedObject);
 	}
 
-//	IEnumerator goBack()
-//	{
-//		
-//		yield return new WaitForSeconds(2);
-//	}
+	IEnumerator goNext()
+	{
+		
+		animator [1].SetTrigger ("fadeOut");
+	
+		yield return new WaitForSeconds (1.5f);
+
+		_enable.SetActive (true);
+		eventSystem.SetSelectedGameObject (_select);
+		_disable.SetActive (false);
+
+
+		yield return new WaitForSecondsRealtime (2);
+		StopCoroutine ("goNext");
+
+		yield return new WaitForEndOfFrame();
+	}
+
+	IEnumerator goBack()
+	{
+		
+			animator[1].SetTrigger ("fadeOut");
+			//animator.Play(0);
+			
+
+			yield return new WaitForSeconds (1.5f);
+			
+			_goBackObject.transform.parent.gameObject.SetActive (true);
+			eventSystem.SetSelectedGameObject (_goBackObject);
+			gameObject.transform.parent.gameObject.SetActive (false);
+			
+
+			yield return new WaitForSeconds (2);
+			StopCoroutine ("goBack");
+
+	}
 }
