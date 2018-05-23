e086c1fdf5a160dfd812e85ebd2fc90422945d9b
LAPTOP-UBUK1R6P\Pascal <pascal-diroll@gmx.de>
2018-05-23 12:51:16 +0200

    loadscene

---
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/WinScreenController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreenController : MonoBehaviour {
+
+	[SerializeField]
+	private LoadSceneManager _scene_manager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("joystick button 9"))
+		{
+			for(int i = 0; i< 4; i++)
+			{
+				GameManager.used_character [i] = 0;
+			}
+			_scene_manager.LoadCharacterSelect();
+		}
+			
+	}
+}
