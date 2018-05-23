fcdf6b146884cee7b11aa1b02c6e46f727a61604
LAPTOP-UBUK1R6P\Pascal <pascal-diroll@gmx.de>
2018-05-23 23:56:31 +0200

    menu tweaks

---
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/Backgroundmusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroundmusic : MonoBehaviour {
+	[SerializeField]
+	private AudioClip _clip;
+	private bool _plays;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+		GetComponent<AudioSource> ().Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.active_characters != null && !_plays) 
+		{
+			
+			GetComponent<AudioSource> ().clip = _clip;
+			GetComponent<AudioSource> ().Play(1);
+			_plays = true;
+		}
+	}
+}
---
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ButtonAudio : MonoBehaviour {
+	
+	private AudioSource[] _my_audiosource;
+
+	[SerializeField]
+	private AudioClip[] _clips;
+
+	private float[] cooldown;
+	// Use this for initialization
+	void Start () {
+		cooldown = new float[_clips.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ((Input.GetAxis ("Vertical") > 0.1 || Input.GetAxis ("Vertical") < -0.1) && cooldown [0] < 0) {
+			_my_audiosource = GetComponents<AudioSource> ();
+			_my_audiosource [0].PlayOneShot (_clips [0]);
+			cooldown [0] = 0.5f;
+		} else {
+			cooldown [0] -= Time.deltaTime;
+		}
+		if ((Input.GetButtonDown ("Submit")  || Input.GetButtonDown ("Cancel") ) && cooldown [1] < 0) {
+			_my_audiosource = GetComponents<AudioSource> ();
+			_my_audiosource [0].PlayOneShot (_clips [1]);
+			cooldown [1] = 1.5f;
+		} else {
+			cooldown [1] -= Time.deltaTime;
+		}
+	}
+}
