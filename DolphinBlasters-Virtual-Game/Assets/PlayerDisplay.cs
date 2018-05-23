580b09a2bf3a52ac580cc1bcc27f4441dd0be9c0
DESKTOP-P1DU2EF\kraem <kraemerfa@t-online.de>
2018-05-23 11:06:02 +0200

    hud

---
+++ DolphinBlasters-Virtual-Game/Assets/PlayerDisplay.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDisplay : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite _normal_slider_sprite;
+
+    [SerializeField]
+    private Sprite _damaged_slider_sprite;
+
+    [SerializeField]
+    private Sprite _normal_head_sprite;
+
+    [SerializeField]
+    private Sprite _damaged_head_sprite;
+
+    [SerializeField]
+    private Sprite _death_sprite;
+
+    [SerializeField]
+    private Image _slider_background;
+
+    [SerializeField]
+    private Image _head;
+
+    [SerializeField]
+    private CharacterBehaviour _character;
+
+    [SerializeField]
+    private Slider _slider;
+
+    private void Start()
+    {
+        gameObject.SetActive(true);
+        _slider.maxValue = 100f;
+        _slider.minValue = 0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(_character.gameObject.activeSelf == false)
+        {
+            gameObject.SetActive(false);
+        }
+        if(_character.GetGotHit() == true)
+        {
+            _head.sprite = _damaged_head_sprite;
+            _slider_background.sprite = _damaged_slider_sprite;
+        }
+        else
+        {
+            _head.sprite = _normal_head_sprite;
+            _slider_background.sprite = _normal_slider_sprite;
+        }
+        _slider.value = _character.GetPowerLevel();
+	}
+}
